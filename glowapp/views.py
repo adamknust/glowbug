66121c12dd909b72110c865bbb7350af925a06cd
Adam Knust <knust.adam@gmail.com>
2018-03-09 12:55:43 -0600

    map is up and working - now to use it for good!

--- glowapp/views.py
+++ glowapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.template import loader
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+
+def map(request):
+    return render(request, 'glowapp/map.html')
